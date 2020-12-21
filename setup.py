6af126b1ad38cb2a4738a9ecc5602c84eb02d6c4
Miki Tebeka <miki.tebeka@gmail.com>
2020-12-21 19:40:58 +0200

    packaging

---
+++ setup.py
@@ -0,0 +1,52 @@
+from setuptools import setup
+
+
+def version():
+    with open('cue.py') as fp:
+        for line in fp:
+            if line.startswith('__version__'):
+                _, version = line.split('=')
+                return version.replace("'", '').strip()
+
+
+def load_deps(file_name):
+    """Load dependencies from requirements file"""
+    deps = []
+    with open(file_name) as fp:
+        for line in fp:
+            line = line.strip()
+            if not line or line[0] == '#' or line.startswith('-r'):
+                continue
+            deps.append(line)
+    return deps
+
+
+tests_require = load_deps('dev-requirements.txt')
+
+with open('README.md') as fp:
+    long_desc = fp.read()
+
+setup(
+    name='cue',
+    version=version(),
+    description='Python wrapper for https://cuelang.org',
+    long_description=long_desc,
+    long_description_content_type='text/markdown',
+    author='Miki Tebeka',
+    author_email='miki@353solutions.com',
+    license='MIT',
+    url='https://github.com/tebeka/cue',
+    py_modules=['cue'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: MacOS',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Software Development :: Libraries',
+    ],
+    tests_require=tests_require,
+)
